d6389fdc7cbe48108f25f91a34f1802fe7213085
S3NP41-v <93288843+S3NP41-v@users.noreply.github.com>
2022-09-06 00:29:42 +0200

    small re-write, tbh I still didn't test this, but I am currently running the old version and I don't want to stop it now

--- download-wbijam.py
+++ download-wbijam.py
@@ -3,22 +3,31 @@ from wbijam import wbijam
 import argparse
 import requests
 import multiprocessing
+from os import system
+from sys import platform, stdout
 
 
-def _download(data: tuple) -> None:  # I/O
-    clink = data[0]
-    k = data[1]
-    args = data[2]
-
+def download(k: int, clink: str, quality: str, path: str, list_only: bool) -> str:  # I/O + returning raw link
     c = CDA(clink)
-    raw_link = c.getRaw(ql=args.q)
+    raw = c.getRaw(ql=quality)
+    filename = f"{k}-{c.playerID}.mp4"
 
-    print(f"#{k} downloading data from {clink} [{raw_link}] ...")  # debug
-    r = requests.get(raw_link)
+    print(f"\x1b[{str(k + 2)};0H\x1b[2KDownloading:  {clink} -> {path}{filename}", end='')
+    stdout.flush()
 
-    print(f"#{k} creating file: " + args.o + '/' + f"{k}-{c.playerID}.mp4")  # debug
-    with open(args.o + '/' + f"{k}-{c.playerID}.mp4", "wb") as f:
-        f.write(r.content)
+    if not list_only:
+        data = requests.get(raw)
+        print(f"\x1b[{str(k+2)};0H\x1b[2KWriting file: {clink} -> {path}{filename}", end='')
+        stdout.flush()
+        with open(f"{path}/{filename}", "wb") as f:
+            f.write(data.content)
+
+    # otherwise only return raw link
+
+    print(f"\x1b[{str(k+2)};0H\x1b[2KCompleted:    {clink} -> {path}{filename}", end='')
+    stdout.flush()
+
+    return raw
 
 
 if __name__ == '__main__':
@@ -29,8 +38,9 @@ if __name__ == '__main__':
     parser.add_argument("-q", required=False, choices=["hd", "sd", "lq", "vl"], help="specify the quality to download [default 'hd']", default="hd")
     parser.add_argument("-o", type=str, required=False, help="specify the path to download to")
 
-    parser.add_argument("-t", type=int, required=False, help="amount of threads to use while downloading [default 4]", default=4)
+    parser.add_argument("-t", type=int, required=False, help="amount of threads to use while downloading [default 1]", default=1)
 
+    parser.add_argument("-l", type=bool, required=False, help="just create list as well as alt-list, dont download", default=False)
     args = parser.parse_args()
 
     # some checks
@@ -44,9 +54,22 @@ if __name__ == '__main__':
         args.o = "./"
 
     w = wbijam(main_link=args.m, series=args.s)
+    clinks = w.get_cLinks()
+    # making a list.txt per standard of my storage
+    with open(args.o + "/" + "list.txt", "w") as f:
+        f.write('\n'.join(clinks))
 
     with multiprocessing.Pool(args.t) as pool:
-        clinks = w.get_cLinks()
-        # TODO: this is just awful, re-do this
-        pool.map(_download, zip(clinks, list(range(len(clinks))), [args] * len(clinks)), chunksize=1)
+        if "win" in platform:
+            system("cls")
+        else:
+            system("clear")
 
+        vals = list(zip(range(len(clinks)), clinks, [args.q] * len(clinks), [args.o] * len(clinks), [args.l] * len(clinks)))
+        raw_links = pool.starmap(download, vals)
+
+    # making an alt-list.txt per standard of my storage
+    with open(args.o + "/" + "alt-list.txt", "w") as f:
+        f.write('\n'.join(raw_links))
+
+    print(f"\x1b[{len(clinks) + 2};0HDone!")
