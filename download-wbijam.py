3d97708d7d48c41a209d1c033c3f22960f5e155d
S3NP41-v <93288843+S3NP41-v@users.noreply.github.com>
2022-09-12 02:18:25 +0200

    skipping already downloaded files

--- download-wbijam.py
+++ download-wbijam.py
@@ -4,8 +4,9 @@ import argparse
 import requests
 import multiprocessing
 from os import system
-from sys import platform, stdout
+from sys import platform
 from time import time, sleep
+from msvcrt import kbhit, getch
 
 
 def dummy_download(k: int, clink: str, quality: str, path: str, list_only: bool) -> str:  # I/O + returning raw link
@@ -24,12 +25,12 @@ def dummy_download(k: int, clink: str, quality: str, path: str, list_only: bool)
 
         _done = round(done / 10 ** 6, 1)
         speed = round(_done / max(ctime, 1), 2)
-        print(f"\x1b[{str(k + 2)};0H\x1b[2K[Dummy] Downloading: [...]{clink.split('/')[-1]} -> {path}{filename} {' ' * (len(str(_filesize)) - len(str(_done)))} {_done}MB/{_filesize}MB ~{speed}MB/s", end='')
-        stdout.flush()
+        print(f"\x1b[{str(k + 2)};0H\x1b[2K[Dummy] Downloading: [...]{clink.split('/')[-1]} -> {path}{filename} {' ' * (len(str(_filesize)) - len(str(_done)))} {_done}MB/{_filesize}MB ~{speed}MB/s", end='', flush=True)
+        # stdout.flush()
 
     total = round((time() - start) / 60, 2)
-    print(f"\x1b[{str(k + 2)};0H\x1b[2K[Dummy] Completed: {clink} -> {path}{filename} Total Time: {total}m {' ' * (len(str(total)) - len(str(speed)))} Speed: {speed}MB/s", end='')
-    stdout.flush()
+    print(f"\x1b[{str(k + 2)};0H\x1b[2K[Dummy] Completed: {clink} -> {path}{filename} Total Time: {total}m {' ' * (len(str(total)) - len(str(speed)))} Speed: {speed}MB/s", end='', flush=True)
+    # stdout.flush()
 
     return raw
 
@@ -38,6 +39,14 @@ def download(k: int, clink: str, quality: str, path: str, list_only: bool) -> st
     c = CDA(clink)
     raw = c.getRaw(ql=quality)
     filename = f"{k}-{c.playerID}.mp4"
+
+    try:  # info: skipping already fully downloaded files
+        open(f"{path}/{filename}", 'x')
+    except FileExistsError:  # if the file exists
+        with open(f"{path}/{filename}", 'rb') as f:
+            if len(f.read()) == int(requests.head(raw).headers.get("content-length")):  # and if it is fully downloaded
+                # using the list_only to skip downloading a file again
+                list_only = True
 
     if not list_only:
         raw_data = requests.get(raw, stream=True)
@@ -54,29 +63,33 @@ def download(k: int, clink: str, quality: str, path: str, list_only: bool) -> st
                 _done = round(done / 10**6, 1)
                 speed = round(_done / max(ctime, 1), 2)
 
-                print(f"\x1b[{str(k + 2)};0H\x1b[2KDownloading: [...]{clink.split('/')[-1]} -> {path}{filename} {' ' * ((len(str(_filesize))+1) - len(str(_done)))} {_done}MB/{_filesize}MB ~{speed}MB/s", end='')
-                stdout.flush()
+                print(f"\x1b[{str(k + 2)};0H\x1b[2KDownloading: [...]{clink.split('/')[-1]} -> {path}{filename} {' ' * ((len(str(_filesize))+1) - len(str(_done)))} {_done}MB/{_filesize}MB ~{speed}MB/s", end='', flush=True)
+                # stdout.flush()
 
                 file.write(chunk)
     # else: return only raw link
 
-    print(f"\x1b[{str(k+2)};0H\x1b[2KCompleted: {clink} -> {path}{filename} total time: {round((time() - start) / 60, 2)}m", end='')
-    stdout.flush()
+    print(f"\x1b[{str(k+2)};0H\x1b[2KCompleted: {clink} -> {path}{filename} total time: {round((time() - start) / 60, 2)}m", end='', flush=True)
+    # stdout.flush()
 
     return raw
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description="download entire series from wbijam.pl")
-    parser.add_argument("-m", type=str, required=True, help="specify the main link, example: https://danmachi.wbijam.pl")
+    parser.add_argument("-m", type=str, required=True,
+                        help="specify the main link, example: https://danmachi.wbijam.pl")
     parser.add_argument("-s", type=str, required=True, help="specify the season, example: pierwsza_seria.html")
 
-    parser.add_argument("-q", required=False, choices=["hd", "sd", "lq", "vl"], help="specify the quality to download [default 'hd']", default="hd")
+    parser.add_argument("-q", required=False, choices=["hd", "sd", "lq", "vl"],
+                        help="specify the quality to download [default 'hd']", default="hd")
     parser.add_argument("-o", type=str, required=False, help="specify the path to download to")
 
-    parser.add_argument("-t", type=int, required=False, help="amount of threads to use while downloading [default 1]", default=1)
+    parser.add_argument("-t", type=int, required=False, help="amount of threads to use while downloading [default 1]",
+                        default=1)
 
-    parser.add_argument("-l", type=bool, required=False, help="just create list as well as alt-list, dont download", default=False)
+    parser.add_argument("-l", type=bool, required=False, help="just create list as well as alt-list, dont download",
+                        default=False)
     parser.add_argument("-b", type=bool, required=False, help="test downloading", default=False)
     args = parser.parse_args()
 
@@ -103,7 +116,8 @@ if __name__ == '__main__':
         else:
             system("clear")
 
-        vals = list(zip(range(len(clinks)), clinks, [args.q] * len(clinks), [args.o] * len(clinks), [args.l] * len(clinks)))
+        vals = list(
+            zip(range(len(clinks)), clinks, [args.q] * len(clinks), [args.o] * len(clinks), [args.l] * len(clinks)))
 
         main_start = time()
         if not args.b:
@@ -111,8 +125,12 @@ if __name__ == '__main__':
         else:
             raw_links = pool.starmap(dummy_download, vals)
 
+
     # making an alt-list.txt per standard of my storage
     with open(args.o + "/" + "alt-list.txt", "w") as f:
         f.write('\n'.join(raw_links))
 
     print(f"\x1b[{len(clinks) + 2};0HDone!, All Total Time: {round((time() - main_start) / 60, 2)}m")
+
+if __name__ == '__main__':
+    main()
